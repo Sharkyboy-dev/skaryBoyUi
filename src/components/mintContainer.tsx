d29c3b47e9ed9a69a5cb8872e406d42e7a4a40ce
SharkyBoy <sharkyboycoin@gmail.com>
2025-04-06 16:13:33 -0400

    Update mintContainer.tsx

--- src/components/mintContainer.tsx
+++ src/components/mintContainer.tsx
@@ -6,52 +6,61 @@ import Link from "next/link";
 
 const MintContainer = () => {
   return (
-    <div className={styles.MintDiv}>
-      <div className={styles.ContentContainer}>
-        {/* LEFT SIDE */}
-        <div className={styles.ContentLeft}>
-          <img src="/logoGif.gif" alt="SharkyBoy" />
-          <Link href="https://mint.sharkyboy.com" target="_blank">
-            <button>Mint Now</button>
-          </Link>
-        </div>
+    <section className="comic-panel">
+      <div className={styles.MintDiv}>
+        <div className={styles.ContentContainer}>
+          {/* LEFT SIDE */}
+          <div className={styles.ContentLeft}>
+            <img src="/logoGif.gif" alt="SharkyBoy" />
+            <Link href="https://mint.sharkyboy.com" target="_blank">
+              <button className={styles.mintButton}>Mint Now</button>
+            </Link>
+          </div>
 
-        {/* RIGHT SIDE */}
-        <div className={styles.ContentRight}>
-          <div className={styles.DescDiv}>
-            <span>
-              OWN THE STORY. SHAPE THE FUTURE.
-              <br />
-              JOIN THE SHARKYGANG.
-            </span>
+          {/* RIGHT SIDE */}
+          <div className={styles.ContentRight}>
+            <div className={styles.DescDiv}>
+              <span
+                style={{
+                  fontFamily: "var(--comic-title)",
+                  fontSize: "2em",
+                  lineHeight: "1.3",
+                  textAlign: "center",
+                }}
+              >
+                OWN THE STORY. SHAPE THE FUTURE.
+                <br />
+                JOIN THE SHARKYGANG.
+              </span>
 
-            <div className={styles.ButtonsContainer}>
-              <Link href="#origin">
-                <button>Origin Story</button>
-              </Link>
-              <Link href="#roadmap">
-                <button>Roadmap</button>
-              </Link>
-            </div>
+              <div className={styles.ButtonsContainer}>
+                <Link href="#origin">
+                  <button>Origin Story</button>
+                </Link>
+                <Link href="#roadmap">
+                  <button>Roadmap</button>
+                </Link>
+              </div>
 
-            <div className={styles.IconsContainer}>
-              <Link href="https://t.co/2PyAgbMLEj" target="_blank">
-                <FaTelegram />
-              </Link>
-              <Link href="http://instagram.com/sharkyboy_nft" target="_blank">
-                <FaInstagram />
-              </Link>
-              <Link href="https://github.com/Sharkyboy-dev" target="_blank">
-                <FaGithub />
-              </Link>
-              <Link href="https://x.com/sharkyboy_nft" target="_blank">
-                <FaXTwitter />
-              </Link>
+              <div className={styles.IconsContainer}>
+                <Link href="https://t.co/2PyAgbMLEj" target="_blank">
+                  <FaTelegram />
+                </Link>
+                <Link href="http://instagram.com/sharkyboy_nft" target="_blank">
+                  <FaInstagram />
+                </Link>
+                <Link href="https://github.com/Sharkyboy-dev" target="_blank">
+                  <FaGithub />
+                </Link>
+                <Link href="https://x.com/sharkyboy_nft" target="_blank">
+                  <FaXTwitter />
+                </Link>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
