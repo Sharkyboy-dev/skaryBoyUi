0d48545e4745bcf6bb3fd29f1bcd28ad9cd66fe5
SharkyBoy <sharkyboycoin@gmail.com>
2025-04-06 22:32:28 -0400

    Update NFTGallery.tsx

--- src/app/NFTGallery.tsx
+++ src/app/NFTGallery.tsx
@@ -1,1 +1,29 @@
+"use client";
+import React from "react";
+import styles from "./NFTGallery.module.css";
+
+const nftImages = [
+  "/nfts/nft1.png",
+  "/nfts/nft2.png",
+  "/nfts/nft3.png",
+  "/nfts/nft4.png",
+  "/nfts/nft5.png"
+];
+
+const NFTGallery = () => {
+  return (
+    <div className={styles.galleryContainer}>
+      <div className={styles.scroller}>
+        {nftImages.map((src, i) => (
+          <img key={i} src={src} alt={`SharkyBoy NFT ${i}`} />
+        ))}
+        {nftImages.map((src, i) => (
+          <img key={`loop-${i}`} src={src} alt={`SharkyBoy NFT ${i}`} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default NFTGallery;
 
