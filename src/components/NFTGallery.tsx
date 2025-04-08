21ccf80b859fc9719d069f6e73018b9251c0f45d
SharkyBoy <sharkyboycoin@gmail.com>
2025-04-07 23:59:58 -0400

    Update NFTGallery.tsx

--- src/components/NFTGallery.tsx
+++ src/components/NFTGallery.tsx
@@ -1,7 +1,24 @@
 "use client";
 import React from "react";
 import styles from "./NFTGallery.module.css";
-import { nftImageMap } from "../data/nftImageMap";
+
+const nftImageMap: { [id: number]: string } = {
+  1: "https://nte2vqorwtt1wdls.public.blob.vercel-storage.com/nfts_clean_final/1-vzKeuJfnuppyxGKgjWGyCdc2kLAD6v.png",
+  2: "https://nte2vqorwtt1wdls.public.blob.vercel-storage.com/nfts_clean_final/2-uSPdPJbS2fytr8YZM1TF3gWphrFOhG.png",
+  10: "https://nte2vqorwtt1wdls.public.blob.vercel-storage.com/nfts_clean_final/10-n9HVnl6ylkOVkGDnxEX9Ujpdqz22lv.png",
+  11: "https://nte2vqorwtt1wdls.public.blob.vercel-storage.com/nfts_clean_final/11-zzmhK6I7doc4nyjOBKRs0dKAy0Fp6q.png",
+  12: "https://nte2vqorwtt1wdls.public.blob.vercel-storage.com/nfts_clean_final/12-FgdXGIVGwDSNiLKuKIlTBy4SckH0jL.png",
+  13: "https://nte2vqorwtt1wdls.public.blob.vercel-storage.com/nfts_clean_final/13-UUo9mgzhu7aXYExvtX3NzD2j2S8FKV.png",
+  14: "https://nte2vqorwtt1wdls.public.blob.vercel-storage.com/nfts_clean_final/14-VTXjzQEis0zfubXksSgWlyaHiUsj86.png",
+  15: "https://nte2vqorwtt1wdls.public.blob.vercel-storage.com/nfts_clean_final/15-li7ooX05u7OdgmSzSpmp034Jsmha4N.png",
+  16: "https://nte2vqorwtt1wdls.public.blob.vercel-storage.com/nfts_clean_final/16-pv5c6vIXDnmIN1MUvSHnCoDTsUOZw3.png",
+  17: "https://nte2vqorwtt1wdls.public.blob.vercel-storage.com/nfts_clean_final/17-bNoI74JWcT4DM3JXoRsNTlVKkzyyZB.png",
+  18: "https://nte2vqorwtt1wdls.public.blob.vercel-storage.com/nfts_clean_final/18-Veu8TUXAswPFaPcisi4sF10e4UBfAs.png",
+  19: "https://nte2vqorwtt1wdls.public.blob.vercel-storage.com/nfts_clean_final/19-HYAmWtGWjSt3K9p9AVyeCUbgd5WRPR.png",
+  20: "https://nte2vqorwtt1wdls.public.blob.vercel-storage.com/nfts_clean_final/20-4cDu7eUxoQgMRRoNww408O6rGFUPDN.png",
+  21: "https://nte2vqorwtt1wdls.public.blob.vercel-storage.com/nfts_clean_final/21-BkaMoLQ0GG3KH4TFaQmNF5ysXrIqid.png",
+  22: "https://nte2vqorwtt1wdls.public.blob.vercel-storage.com/nfts_clean_final/22-tJjbRM89QKSQnlXV7TWFh87sO6wkhR.png"
+};
 
 const NFTGallery = () => {
   const images = Object.entries(nftImageMap);
