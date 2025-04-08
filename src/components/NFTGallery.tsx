0d2b53d3f8dffde5e8db9e05ff61fcaad19c72c3
SharkyBoy <sharkyboycoin@gmail.com>
2025-04-07 23:16:54 -0400

    Update NFTGallery.tsx

--- src/components/NFTGallery.tsx
+++ src/components/NFTGallery.tsx
@@ -1,27 +1,21 @@
 "use client";
 import React from "react";
 import styles from "./NFTGallery.module.css";
-
-const TOTAL_IMAGES = 100;
-const BASE_URL = "https://nte2vqorwtt1wdls.public.blob.vercel-storage.com/nfts_clean_final";
+import { nftImageMap } from "../data/nftImageMap";
 
 const NFTGallery = () => {
-  const images = Array.from({ length: TOTAL_IMAGES }, (_, i) => {
-    return `${BASE_URL}/${i + 1}.png`;
-  });
+  const images = Object.entries(nftImageMap);
 
   return (
     <div className={styles.galleryWrapper}>
       <div className={styles.scrollTrack}>
-        {images.map((src, index) => (
+        {images.map(([id, src]) => (
           <img
-            key={index}
+            key={id}
             src={src}
-            alt={`SharkyBoy #${index + 1}`}
+            alt={`SharkyBoy #${id}`}
             className={styles.nftImage}
-            onError={(e) => {
-              e.currentTarget.style.display = "none";
-            }}
+            onError={(e) => (e.currentTarget.style.display = "none")}
           />
         ))}
       </div>
